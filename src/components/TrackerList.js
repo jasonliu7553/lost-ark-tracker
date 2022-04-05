d412a0e9fe54926cf61baa96a861b7f58582bbdd
jasonliu7553 <liuwenjie7553@gmail.com>
2022-04-04 22:00:52 -0400

    got timer to work

--- src/components/TrackerList.js
+++ src/components/TrackerList.js
@@ -1,33 +1,80 @@
 import React from 'react'
+import { useState } from 'react'
 
 const TrackerList = ({ schedEvents }) => {
 
     const today = new Date()
+    const [eventTimers, setEventTimers] = useState()
+
 
     //filter out the events that are not on the reminder list, or already visited
-    const trackedEvents = schedEvents.filter((event) => (
+    var trackedEvents = schedEvents.filter((event) => (
         event.reminder == true && event.visited == false
     ))
 
-    //returns the next time event will happen as a string
-    const formatTime = (event) => {
+    const nextDay = (event) => {
+        today.getHours()
+    }
+
+    //return the next hour the boss spawns or -1 if there are no more spawn hours
+    const nextHour = (event, next) => {
         const hours = event.hour.filter((hour) => (
             hour > today.getHours()
         ))
+
+        if (next === true && hours.length > 1) {
+            return hours[1]
+        } else if (next === true) {
+            return -1
+        }
+        else if (hours.length > 0) {
+            return hours[0]
+        }
+        else {
+            return -1
+        }
+
+    }
+
+    const updateTimer = (event) => {
+        var day = new Date()
+        var hour
+        var minute
+        var sec = 60 - day.getSeconds()
+
+        if (event.minute < day.minute && event.minute != 0) {
+            hour = nextHour(event, true)
+            minute = event.minute
+        }
+        else {
+            hour = nextHour(event, false) - 1
+            minute = 60
+        }
+
+        event.timer = [hour - day.getHours(), Math.abs(minute - day.getMinutes()), sec]
+
+        var run = setInterval(() => (setEventTimers(event.timer)), 1000)
+
+    }
+
+    //returns the next time event will happen as a string
+    const formatTime = (event) => {
         const minute = event.minute
+        var hour
+        if (today.getMinutes > minute && minute != 0) {
+            hour = nextHour(event, true)
+        }
+        else {
+            hour = nextHour(event, false)
+        }
 
-        var output = ` ${hours[0]}:${minute[0]}`
+        var output = ` ${hour}:${minute}`
 
-        if (hours.length < 1) {
+        if (hour === -1) {
             output = 'no more spawns'
         }
-        else if (hours.length < 2 && minute[0] < today.getMinutes()) {
-            output = 'no more spawns'
-        }
-        else if (minute[0] < today.getMinutes() && minute[0] != 0) {
-            output = `${hours[1]}:${minute[0]}`
-        }
 
+        updateTimer(event)
 
         return output
     }
@@ -39,7 +86,7 @@ const TrackerList = ({ schedEvents }) => {
                     {event.event}
                 </h3>
                 <h4>
-                    Next spawn: {formatTime(event)}
+                    Next spawn: {formatTime(event)},    {event.timer[0]}:{event.timer[1]}:{event.timer[2]}
                 </h4>
             </div>
         ))
